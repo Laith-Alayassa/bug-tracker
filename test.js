797577efc3b2d4eee3a06d7738aad91cb2bd6aff
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-06-24 23:25:56 -0500

    testing new js file

--- test.js
+++ test.js
@@ -2,6 +2,21 @@ import express from "express";
 const app = express()
 const port = process.env.PORT
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
