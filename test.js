047b2c42bc2a094578c208adc8cbd636afbbb7ab
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-06-24 23:18:05 -0500

    testing new js file

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 import express from "express";
-const app = express()
-const port = process.env.PORT;
+import ejs from "ejs";
+import bodyParser from "body-parser";
+import moment from "moment";
+import mongoose from "mongoose";
+import path from "path";
+import { User, Bug, bugSchema } from "./models/models.js";
+import { password } from "./password.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// setup
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get('/' , (req, res) => {
+    res.send('test.js bb')
 })
 
 app.listen(port, () => {
