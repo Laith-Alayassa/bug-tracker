c8d449dce64ad8096d0fcc352d292954e7588ac1
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-06-24 23:23:57 -0500

    testing new js file

--- test.js
+++ test.js
@@ -1,19 +1,11 @@
 import express from "express";
+const app = express()
+const port = process.env.PORT
 
-
-// setup
-const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.get('/' , (req, res) => {
-    res.send('test.js bb')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-
-const PORT = process.env.PORT || 3000;
-
-
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
