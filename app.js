9d60b9797b2671afda73e55d930418d4c78eaa03
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-07-10 14:24:29 -0500

    💪 created get bugs and create bug API

--- app.js
+++ app.js
@@ -557,6 +557,70 @@ app.get("/plot", (req, res) => {
   });
 });
 
+
+
+
+// RESTFUL API 
+app.get("/api/all-bugs", (req, res) => {
+  Bug.find({}, (err, found) => {
+    if (err) {
+      res.send('303')
+    } else {
+      res.send({
+        "request" : "GET",
+        "bugs" : found
+      })
+    }
+  });
+});
+
+
+app.post("/api/new-bug", (req, res) => {
+  const newBugName = req.query.name;
+  const newBugDescription = req.query.description;
+  const newBugImportance = req.query.importance;
+  const newBugProgress = req.query.progress;
+  let newBugProject = req.query.project;
+
+  console.log(newBugName,
+    newBugDescription,
+    newBugImportance,
+    newBugProgress,
+    newBugProject);
+  let newBugDuty;
+  // finding duty using the id of the user
+  User.findById(req.query.duty, (err, foundUser) => {
+    if (err) {
+      console.log("coudlnt find user with that id");
+    } else {
+      console.log(foundUser + "the duty ---------------=-");
+      let newBug = Bug({
+        name: newBugName,
+        description: newBugDescription,
+        importance: newBugImportance,
+        duty: foundUser,
+        progress: newBugProgress,
+        project: newBugProject,
+        time: new Date(),
+      });
+
+      newBug.save((err) => {
+        if (err) {
+          console.log("error saving new bug");
+        } else {
+          res.send({
+            'status' : 'success'
+          });
+        }
+      });
+    }
+  });
+});
+
+
+
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
